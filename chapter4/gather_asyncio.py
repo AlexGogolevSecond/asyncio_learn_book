05a2c3db5d0af2742135641fa77e4632d279e566
alex <aaa@mail.ru>
2023-08-09 22:26:07 +0500

    пробую gather 1

---
+++ chapter4/gather_asyncio.py
@@ -0,0 +1,19 @@
+import asyncio
+from time import perf_counter
+
+
+async def sl(n: int) -> None:
+    await asyncio.sleep(n)
+
+
+async def main():
+    start = perf_counter()
+    task1 = sl(5)
+    task2 = sl(3)
+
+    await asyncio.gather(task1, task2)
+
+    print(perf_counter() - start)
+
+
+asyncio.run(main())
