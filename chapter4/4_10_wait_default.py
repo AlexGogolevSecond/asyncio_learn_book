7733e6d5a1ee318b93d8d9dd841be80fb5e3fd5f
alex <aaa@mail.ru>
2023-08-10 23:19:17 +0500

    wait exceptions2

--- chapter4/4_10_wait_default.py
+++ chapter4/4_10_wait_default.py
@@ -5,12 +5,12 @@ from util import async_timed
 from util import fetch_status
 
 
-@async_timed()
+# @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
         fetchers = [asyncio.create_task(fetch_status(session, 'https://example.com')),
-                    asyncio.create_task(fetch_status(session, 'https://example.com')),
-                    asyncio.create_task(fetch_status(session, 'https22://example.com'))]
+                    asyncio.create_task(fetch_status(session, 'https://example.com'))]
+                    # asyncio.create_task(fetch_status(session, 'https22://example.com'))]
         # try:
         done, pending = await asyncio.wait(fetchers)
         print(f'Число завершившихся задач: {len(done)}')
@@ -18,6 +18,11 @@ async def main():
         for done_task in done:
             result = await done_task
             print(result)
+            # if done_task.exception is None:
+            #     print(str(done_task.result()) + 'aaa')
+            # else:
+            #     print(str(done_task.exception()) + 'uuu')
+
         # except Exception as ex:
         #     print(str(ex))
 
