764173b0b821fc3d79f61ec49e110c99b5d5b2b3
alex <aaa@mail.ru>
2023-08-13 02:12:53 +0500

    multiproc3

--- chapter6/6_2_pool_processes.py
+++ chapter6/6_2_pool_processes.py
@@ -1,13 +1,32 @@
 from multiprocessing import Pool
+import time
 
 
 def say_hello(name: str) -> str:
     return f'Привет, {name}'
 
 
+def count(count_to: int) -> int:
+    start = time.perf_counter()
+    counter = 0
+    while counter < count_to:
+        counter = counter + 1
+    end = time.perf_counter()
+    print(f'Закончен подсчет до {count_to} за время {end-start}')
+
+    return counter
+
+
 if __name__ == "__main__":
+    st = time.perf_counter()
     with Pool() as process_pool:
-        hi_jeff = process_pool.apply(say_hello, args=('Jeff',))
-        hi_john = process_pool.apply(say_hello, args=('John',))
-        print(hi_jeff)
-        print(hi_john)
+        # hi_jeff = process_pool.apply(say_hello, args=('Jeff',))
+        # hi_john = process_pool.apply(say_hello, args=('John',))
+        # print(hi_jeff)
+        # print(hi_john)
+        p1 = process_pool.apply(count, args=(10000000,))
+        p2 = process_pool.apply(count, args=(20000000,))
+        print(f'{p1=}')
+        print(f'{p2=}')
+    
+    print(time.perf_counter() - st)
