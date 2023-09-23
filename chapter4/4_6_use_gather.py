813a02029612c835f6226d5152c70192812850c6
alex <you@example.com>
2023-09-23 22:29:48 +0500

    gather

--- chapter4/4_6_use_gather.py
+++ chapter4/4_6_use_gather.py
@@ -5,8 +5,7 @@ from aiohttp import ClientSession
 import time
 
 
-async def fetch_status(session: ClientSession, url: str, delay: int = 0) -> int:
-    await asyncio.sleep(delay)
+async def fetch_status(session: ClientSession, url: str) -> int:
     async with session.get(url) as result:
         return result.status
 
