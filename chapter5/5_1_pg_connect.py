3d17f2eb12550a1ddfc222532365ea3cbb7b9fc1
alex <you@example.com>
2023-09-27 22:37:25 +0500

    5_1

--- chapter5/5_1_pg_connect.py
+++ chapter5/5_1_pg_connect.py
@@ -4,9 +4,9 @@ import asyncio
 
 async def main():
     connection = await asyncpg.connect(host='127.0.0.1',
-                                       port=5432,
+                                       port=7432,
                                        user='alex',
-                                       database='postgres',
+                                       database='somebase',
                                        password='614007')
     version = connection.get_server_version()
     print(f'Подключено! Версия Postgres равна {version}')
