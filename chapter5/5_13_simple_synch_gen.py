0cec17c63e1ac99c8c582f70e0719bf6906cad89
alex <you@example.com>
2023-10-01 23:27:07 +0500

    rename and added synch gen

---
+++ chapter5/5_13_simple_synch_gen.py
@@ -0,0 +1,14 @@
+def positive_integers(until: int):
+    for integer in range(until):
+        yield integer
+
+
+positive_iterator = positive_integers(2)
+
+# 1-ый способ
+#print(next(positive_iterator))
+#print(next(positive_iterator))
+
+# 2-й способ
+for el in positive_iterator:
+    print(el)
