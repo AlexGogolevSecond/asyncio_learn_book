ec3a8a21c5afb5ae3ba6337a94ddb9b967576f87
alex <you@example.com>
2023-10-02 22:53:59 +0500

    change cursor forward

--- chapter5/5_16_cursor_forward.py
+++ chapter5/5_16_cursor_forward.py
@@ -8,20 +8,16 @@ async def main():
                                        user='alex',
                                        database='products',
                                        password='614007')
-    
+
     async with connection.transaction():
         query = 'SELECT product_id, product_name from product'
         cursor = await connection.cursor(query)
 
-        async for product in cursor:
-            print(product)
-        '''
         await cursor.forward(500)
         products = await cursor.fetch(100)
         for product in products:
             print(product)
-        '''
-            
+
     await connection.close()
 
 
