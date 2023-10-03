b43526d86503db55bb40aa0cf3a020493180d59c
alex <you@example.com>
2023-10-03 21:07:27 +0500

    multiproc

--- chapter6/6_1_multiproc.py
+++ chapter6/6_1_multiproc.py
@@ -6,7 +6,7 @@ def count(count_to: int) -> int:
     start = time.perf_counter()
     counter = 0
     while counter < count_to:
-        counter = counter + 1
+        counter += 1
     end = time.perf_counter()
     print(f'Закончен подсчет до {count_to} за время {end-start}')
 
@@ -15,8 +15,8 @@ def count(count_to: int) -> int:
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    to_one_hundred_million = Process(target=count, args=(10000000,))
-    to_two_hundred_million = Process(target=count, args=(20000000,))
+    to_one_hundred_million = Process(target=count, args=(100_000_000,))
+    to_two_hundred_million = Process(target=count, args=(200_000_000,))
 
     to_one_hundred_million.start()
     to_two_hundred_million.start()
