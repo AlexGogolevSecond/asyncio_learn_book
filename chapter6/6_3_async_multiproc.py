539fdb2f8308422756dc3ba95c406e77181e986e
alex <you@example.com>
2023-10-03 23:34:43 +0500

    типа асинхронно мультипроц

--- chapter6/6_3_async_multiproc.py
+++ chapter6/6_3_async_multiproc.py
@@ -24,12 +24,16 @@ if __name__ == "__main__":
         # hi_john = process_pool.apply(say_hello, args=('John',))
         # print(hi_jeff)
         # print(hi_john)
-        p1 = process_pool.apply_async(count, args=(10000000,))
-        p2 = process_pool.apply_async(count, args=(20000000,))
-        p3 = process_pool.apply_async(count, args=(40000000,))
-        p4 = process_pool.apply_async(count, args=(20000000,))
-        p5 = process_pool.apply_async(count, args=(30000000,))
-        # почему-то без принтов сразу сбрасывается, как будто нет join по процессам и главный процесс никго не ждёт
+        p1 = process_pool.apply_async(count, args=(100_000_000,))
+        p2 = process_pool.apply_async(count, args=(20_000_000,))
+        p3 = process_pool.apply_async(count, args=(40_000_000,))
+        p4 = process_pool.apply_async(count, args=(20_000_000,))
+        p5 = process_pool.apply_async(count, args=(30_000_000,))
+        # почему-то без принтов сразу сбрасывается, как будто нет join по процессам и главный процесс никого не ждёт
+        # ответ ниже
+
+        print(f'type(p1): {type(p1)}')  # тут вернётся class 'multiprocessing.pool.ApplyResult'
+        # метод get у ApplyResult возвращает результат
         print(f'{p1.get()=}')
         print(f'{p2.get()=}')
         print(f'{p3.get()=}')
@@ -37,3 +41,5 @@ if __name__ == "__main__":
         print(f'{p5.get()=}')
     
     print(time.perf_counter() - st)
+
+    # ну так себе - пользоваться таким неудобно
