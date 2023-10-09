6c9b9ec74018987c6dd97efb6ab172adb9a55004
alex <lalala@ya.ru>
2023-10-09 18:18:15 +0500

    6_5

--- chapter6/6_5_pool_process_asyncio.py
+++ chapter6/6_5_pool_process_asyncio.py
@@ -3,20 +3,22 @@ from asyncio.events import AbstractEventLoop
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from typing import List
+from time import perf_counter
 
 
 def count(count_to: int) -> int:
+    start = perf_counter()
     counter = 0
     while counter < count_to:
         counter += 1
-
-    return counter
+    end = perf_counter()
+    print(f'Закончен подсчет до {count_to} за время {end-start}')
 
 
 async def main():
     with ProcessPoolExecutor() as process_pool:
         loop: AbstractEventLoop = asyncio.get_running_loop()
-        nums = [1, 3, 5, 22, 100000000]
+        nums = [1, 22, 100_000_000, 5, 3]
         calls: List[partial[int]] = [partial(count, num) for num in nums]
         call_coros = []
         for call in calls:
