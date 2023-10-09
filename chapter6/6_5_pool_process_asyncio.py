99bc89520f139bf5bc9ae19a702d4a0945f76d4e
alex <lalala@ya.ru>
2023-10-09 22:57:23 +0500

    comment to 6_5

--- chapter6/6_5_pool_process_asyncio.py
+++ chapter6/6_5_pool_process_asyncio.py
@@ -7,6 +7,7 @@ from time import perf_counter
 
 
 def count(count_to: int) -> int:
+    '''т.е. метод м.б. даже не синхронным'''    
     start = perf_counter()
     counter = 0
     while counter < count_to:
