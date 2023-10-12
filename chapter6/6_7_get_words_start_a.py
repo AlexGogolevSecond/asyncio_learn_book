2eec459eb0f00a6991b1fb9fe6ab0e35bf0b3670
alex <you@example.com>
2023-10-12 22:50:19 +0500

    6_7 get all time

--- chapter6/6_7_get_words_start_a.py
+++ chapter6/6_7_get_words_start_a.py
@@ -4,6 +4,7 @@ import os
 file_size = os.path.getsize('chapter6/googlebooks-eng-all-1gram-20120701-a')
 print(f'Размер файла: {file_size} байт ({round((file_size / (1024*1024*1024)), 2)} Гбайт)')
 
+st1 = time.time()
 freqs = {}
 with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as f:
     lines = f.readlines()
@@ -18,4 +19,5 @@ with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as 
             freqs[word] = count
     end = time.time()
 
-    print(f'{end-start:.4f}')
+    print(f'{end-start:.4f} c.')
+    print(f'{end-st1:.4f} c.')
