e4981ab671c28e2799f3692086ec97e3cf2bbfed
alex <you@example.com>
2023-10-14 23:23:58 +0500

    read yield big file

--- chapter6/read_file_with_yield.py
+++ chapter6/read_file_with_yield.py
@@ -1,7 +1,9 @@
 '''
 def read_in_chunks(file_object, chunk_size=1024):
-    """Lazy function (generator) to read a file piece by piece.
-    Default chunk size: 1k."""
+    """
+    Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1k.
+    """
     while True:
         data = file_object.read(chunk_size)
         if not data:
@@ -9,9 +11,9 @@ def read_in_chunks(file_object, chunk_size=1024):
         yield data
 
         
-file = open('chapter6/googlebooks-eng-all-1gram-20120701-a')
-res1 = file.read()  # ~ 3.35 Гб в памяти
-res2 = res1.split('\n')  # разделение строки по разделителю и возврат списка из этой строки
+#file = open('chapter6/googlebooks-eng-all-1gram-20120701-a')
+#res1 = file.read()  # ~ 3.35 Гб в памяти
+#res2 = res1.split('\n')  # разделение строки по разделителю и возврат списка из этой строки
 
 
 with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as f:
