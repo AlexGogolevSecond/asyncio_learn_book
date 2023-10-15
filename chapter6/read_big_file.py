93006766420df5cc2bc7ade05bfcb7b1ce2d92a4
alex <you@example.com>
2023-10-16 00:37:41 +0500

    return and yield

--- chapter6/read_big_file.py
+++ chapter6/read_big_file.py
@@ -12,12 +12,20 @@ def csv_reader2(file_name):  # тут примерно 100 Мб отъедает
         yield row
 
 st = time.perf_counter()
-csv_gen = csv_reader2("chapter6/googlebooks-eng-all-1gram-20120701-a")
-#csv_gen = csv_reader2("chapter6/test_file_for_read.txt")
+file = "chapter6/googlebooks-eng-all-1gram-20120701-a"
+print('before csv_gen = ')
+#file = "chapter6/test_file_for_read.txt"
+#csv_gen = csv_reader(file)
+#csv_gen = csv_reader2(file)
+csv_gen = (row for row in open(file))  # более питонячее формирование выражения-генератора - аналог csv_reader2
 row_count = 0
 
+print('before circle by csv_gen')
 for row in csv_gen:
     row_count += 1
+    if row_count % 1000000 == 0:
+        print(f'row_count: {row_count}')
 
-print(f'csv_reader time: {time.perf_counter() - st}')  # ресурсоёмкий метод ~ 26 сек занимает
+print(f'csv_reader time: {time.perf_counter() - st}')  # ресурсоёмкий метод ~ 35 сек занимает; 
+                                                       # метод с генераторм (или выражение-генератор) ~ 346 сек.
 print(f"Row count is {row_count}")
