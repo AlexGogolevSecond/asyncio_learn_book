73443a5d9ea7901a1daaa9e63f07a523ec83d5a4
alex <aaa@ya.ru>
2023-10-17 07:44:22 +0500

    6_8 pool process map reduce

---
+++ chapter6/6_8_pool_process_map_reduce.py
@@ -0,0 +1,26 @@
+import asyncio
+import concurrent.futures
+import functools
+import time
+from typing import Dict, List
+
+
+def partition(data: List, chunk_size: int) -> List:
+    for i in range(0, len(data), chunk_size):
+        yield data[i:i + chunk_size]
+
+
+def map_frequencies(chunk: List[str]) -> Dict[str, int]:
+counter = {}
+for line in chunk:
+word, _, count, _ = line.split('\t')
+if counter.get(word):
+counter[word] = counter[word] + int(count)
+else:
+counter[word] = int(count)
+return counter
+def merge_dictionaries(first: Dict[str, int],
+second: Dict[str, int]) -> Dict[str, int]:
+merged = first
+for key in second:
+if key in merged:
