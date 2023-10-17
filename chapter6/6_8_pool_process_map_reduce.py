6da931504f302b7a4a9bd231f25d63d911b9b2a3
alex <aaa@ya.ru>
2023-10-17 07:51:03 +0500

    6_8 3

--- chapter6/6_8_pool_process_map_reduce.py
+++ chapter6/6_8_pool_process_map_reduce.py
@@ -26,8 +26,33 @@ def merge_dictionaries(first: Dict[str, int],
     merged = first
     for key in second:
         if key in merged:
+            merged[key] = merged[key] + second[key]
+        else:
+            merged[key] = second[key]
+        return merged
+    
 
-
-
-
+async def main(partition_size: int):
+    with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as f:
+        contents = f.readlines()
+        loop = asyncio.get_running_loop()
+        tasks = []
+        start = time.time()
+        with concurrent.futures.ProcessPoolExecutor() as pool:
+            for chunk in partition(contents, partition_size):
+                tasks.append(loop.run_in_executor(pool,
+                                                  functools.partial(map_frequencies, chunk)))
             
+            intermediate_results = await asyncio.gather(*tasks)
+            final_result = functools.reduce(merge_dictionaries,
+            intermediate_results)
+            print(f"Aardvark встречается {final_result['Aardvark']} раз.")
+            end = time.time()
+            print(f'Время MapReduce: {(end - start):.4f} секунд')
+
+
+if __name__ == "__main__":
+    asyncio.run(main(partition_size=60000))
+
+
+
