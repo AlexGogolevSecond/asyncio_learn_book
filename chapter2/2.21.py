d1069e9be92a1fbbd3a0aefb33c61031d971d358
Aleksandr <a.gogolev@carbonsoft.ru>
2024-05-29 21:49:56 +0500

    2,21

---
+++ chapter2/2.21.py
@@ -0,0 +1,14 @@
+"""custom event loop"""
+import asyncio
+
+
+async def main():
+    print(f'засыпаю на 1 с.')
+    await asyncio.sleep(1)
+    print(f'просыпаюсь после сна 1 с.')
+
+loop = asyncio.new_event_loop()
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.close()
