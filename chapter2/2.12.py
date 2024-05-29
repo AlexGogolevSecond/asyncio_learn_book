43f1f0f18bb9d5b76eb6e027b979cb1f44d48f62
Aleksandr <a.gogolev@carbonsoft.ru>
2024-05-29 19:47:53 +0500

    2,12

--- chapter2/2.12.py
+++ chapter2/2.12.py
@@ -4,10 +4,10 @@ from util import delay
 
 async def main():
     delay_task = asyncio.create_task(delay(2))
-    
+
     try:
         result = await asyncio.wait_for(delay_task, timeout=1)
-        print(result)
+        print(f'{result=}')
     except asyncio.exceptions.TimeoutError:
         print('Тайм-аут!')
         print(f'Задача была снята? {delay_task.cancelled()}')
