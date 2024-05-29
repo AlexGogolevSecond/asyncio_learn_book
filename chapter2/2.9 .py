394efc6a3359b37b75efa0a267e96270bbd85108
Aleksandr <a.gogolev@carbonsoft.ru>
2024-05-29 19:21:01 +0500

    2,9

--- chapter2/2.9 .py
+++ chapter2/2.9 .py
@@ -3,7 +3,9 @@ import time
 
 
 async def sl(n):
+    print(f'засыпаю на {n} с.')
     await asyncio.sleep(n)
+    print(f'проснулся после сна {n} с.')
 
 
 async def main():
@@ -16,6 +18,7 @@ async def main():
     # await asyncio.create_task(sl(4))
     
     await asyncio.gather(t1, t2)  # можно и корутины передавать и задачи
+    # await asyncio.gather(sl(3), sl(4))
 
     finish = time.time()
     print(finish - start)
