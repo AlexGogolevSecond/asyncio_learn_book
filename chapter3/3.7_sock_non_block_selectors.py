269b0a9eb9cba9f14318ca2f7aefa26e2cb38c6b
Aleksandr <a.gogolev@carbonsoft.ru>
2024-05-30 23:36:50 +0500

    3,7

--- chapter3/3.7_sock_non_block_selectors.py
+++ chapter3/3.7_sock_non_block_selectors.py
@@ -5,12 +5,16 @@ from typing import List, Tuple
 
 
 selector = selectors.DefaultSelector()
+
 server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server_address = ('127.0.0.1', 8000)
+
 server_socket.setblocking(False)
 server_socket.bind(server_address)
 server_socket.listen()
+
 selector.register(server_socket, selectors.EVENT_READ)
 
 while True:
