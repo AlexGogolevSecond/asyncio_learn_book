d6cac29788c8d04f9fe31d8f18c01efb6ebb0c73
Aleksandr <a.gogolev@carbonsoft.ru>
2024-05-31 10:03:31 +0500

    2,9,1

---
+++ chapter2/2.9.1.py
@@ -0,0 +1,26 @@
+import asyncio
+import time
+
+
+async def sl(n):
+    print(f'засыпаю на {n} с.')
+    await asyncio.sleep(n)
+    print(f'проснулся после сна {n} с.')
+
+
+async def main():
+    start = time.time()
+    t1 = asyncio.create_task(sl(3))
+    t2 = asyncio.create_task(sl(4))
+    # await t1
+    # await t2
+    # await asyncio.create_task(sl(3))
+    # await asyncio.create_task(sl(4))
+    
+    # await asyncio.gather(t1, t2)  # можно и корутины передавать и задачи
+    # await asyncio.gather(sl(3), sl(4))
+
+    finish = time.time()
+    print(finish - start)
+
+asyncio.run(main())
