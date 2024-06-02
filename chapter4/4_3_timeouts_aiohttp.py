95ade81cfbde40502d54769912cd83f11363f194
Aleksandr <a.gogolev@carbonsoft.ru>
2024-06-02 23:42:02 +0500

    4,3

--- chapter4/4_3_timeouts_aiohttp.py
+++ chapter4/4_3_timeouts_aiohttp.py
@@ -5,15 +5,16 @@ from aiohttp import ClientSession
 
 
 async def fetch_status(session: ClientSession, url: str) -> int:
-    ten_millis = aiohttp.ClientTimeout(total=.1)
+    ten_millis = aiohttp.ClientTimeout(total=1)
     async with session.get(url, timeout=ten_millis) as result:
         return result.status
 
 
 async def main():
-    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+    session_timeout = aiohttp.ClientTimeout(total=5, connect=1)
     async with aiohttp.ClientSession(timeout=session_timeout) as session:
-        await fetch_status(session, 'https://example.com')
+        status = await fetch_status(session, 'https://mail.com')
+        print(f'{status=}')
 
 
 asyncio.run(main())
