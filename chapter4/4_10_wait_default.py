acbd084907e5ce68c62172113cafa8c9e1961dc7
Aleksandr <a.gogolev@carbonsoft.ru>
2024-06-08 23:33:16 +0500

    4,10

--- chapter4/4_10_wait_default.py
+++ chapter4/4_10_wait_default.py
@@ -23,7 +23,7 @@ async def main():
                                                          # режим return_when == ALL_COMPLETED. а в этом случае asyncio.wait
                                                          # не вернется, пока все не завершится
         for done_task in done:
-            result = await done_task
+            result = await done_task  # из-за этого и падает если exception - см. 4.11/4.12
             print(result)
             # if done_task.exception is None:
             #     print(str(done_task.result()) + 'aaa')
