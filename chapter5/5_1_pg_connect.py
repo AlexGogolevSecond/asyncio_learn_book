97fb87a8e76bb1c24c1055a7e01d021d10345bb7
Aleksandr <a.gogolev@carbonsoft.ru>
2024-06-10 23:07:59 +0500

    5,1

--- chapter5/5_1_pg_connect.py
+++ chapter5/5_1_pg_connect.py
@@ -4,10 +4,10 @@ import asyncio
 
 async def main():
     connection = await asyncpg.connect(host='127.0.0.1',
-                                       port=7432,
-                                       user='alex',
+                                       port=19432,
+                                       user='postgres',
                                        database='products',
-                                       password='614007')
+                                       password='password')
     version = connection.get_server_version()
     print(f'Подключено! Версия Postgres равна {version}')
     await connection.close()
