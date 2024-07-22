b1a2d9da2ce556b1397e060d5adb0b58718f04b0
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-23 00:25:38 +0500

    6,7

--- chapter6/6_7_get_words_start_a.py
+++ chapter6/6_7_get_words_start_a.py
@@ -19,12 +19,18 @@ with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as 
             freqs[word] = count
     end = time.time()
 
-    print(f'{end-start:.4f} c.')
-    print(f'{end-st1:.4f} c.')
+    print(f'{end-start:.2f} c.')
+    print(f'{end-st1:.2f} c. - вместе с загрузкой всех строк в память')
 
 '''
 Нужный нам файл можно скачать по адресу https://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-a.gz
 или https://mattfowler.io/data/googlebooks-eng-all-1gram-20120701-a.gz. Любую другую часть
 этого набора данных можно скачать по адресу http://storage.googlea-pis.com/books/ngrams/books/datasetsv2.html
 
+Вывод:
+Размер файла: 1801526075 байт (1.68 Гбайт)
+50.95 c.
+57.88 c. - вместе с загрузкой всех строк в память
+
 '''
+
