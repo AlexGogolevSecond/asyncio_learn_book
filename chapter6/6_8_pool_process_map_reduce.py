d41be5bf71417ec8f56d32dcd167f5eb08c9e101
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-23 23:53:25 +0500

    6,8

--- chapter6/6_8_pool_process_map_reduce.py
+++ chapter6/6_8_pool_process_map_reduce.py
@@ -45,16 +45,57 @@ async def main(partition_size=60000):
             for chunk in partition(contents, partition_size):
                 tasks.append(loop.run_in_executor(pool, functools.partial(map_frequencies, chunk)))
 
-            intermediate_results = await asyncio.gather(*tasks)
+            intermediate_results = await asyncio.gather(*tasks)  # тут ждём все процессы
+            st_reduce = time.time()
             final_result = functools.reduce(merge_dictionaries, intermediate_results)
+            fin_reduce = time.time()
+            print(f'functools.reduce выполнился за: {fin_reduce - st_reduce}')
             # print('-' *25 )
             # print(final_result.keys())
             # print('-' *25 )
             #print(f"Aardvark встречается {final_result.get('Aardvark_')} раз.")  # AFFIX_
-            print(f"AFFIX_ встречается {final_result.get('AFFIX_')} раз.")
+            print(f"AFFIX_ встречается {final_result.get('A.D.A.A.')} раз.")
             end = time.time()
             print(f'Время MapReduce: {(end - start):.4f} секунд')
 
 
 if __name__ == "__main__":
     asyncio.run(main(partition_size=60000))
+
+'''
+Файл содержит:
+
+A'Aang_NOUN	1879	45	5
+A'Aang_NOUN	1882	5	4
+A'Aang_NOUN	1885	1	1
+A'Aang_NOUN	1891	1	1
+A'Aang_NOUN	1899	20	4
+A'Aang_NOUN	1927	3	1
+A'Aang_NOUN	1959	5	2
+A'Aang_NOUN	1962	2	2
+A'Aang_NOUN	1963	1	1
+A'Aang_NOUN	1966	45	13
+A'Aang_NOUN	1967	6	4
+A'Aang_NOUN	1968	5	4
+A'Aang_NOUN	1970	6	2
+A'Aang_NOUN	1975	4	1
+A'Aang_NOUN	2001	1	1
+A'Aang_NOUN	2004	3	1
+A'que_ADJ	1808	1	1
+A'que_ADJ	1849	2	1
+A'que_ADJ	1850	1	1
+A'que_ADJ	1852	4	3
+A'que_ADJ	1854	5	3
+A'que_ADJ	1856	2	1
+A'que_ADJ	1858	4	3
+A'que_ADJ	1862	2	1
+A'que_ADJ	1871	1	1
+A'que_ADJ	1872	2	2
+A'que_ADJ	1873	1	1
+A'que_ADJ	1874	2	2
+A'que_ADJ	1875	3	3
+A'que_ADJ	1877	1	1
+A'que_ADJ	1881	1	1
+A'que_ADJ	1883	1	1
+
+'''
