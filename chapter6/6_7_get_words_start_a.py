f935909cbd3a3e3256a12954331055da3d9de53b
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-23 22:57:43 +0500

    6,7

--- chapter6/6_7_get_words_start_a.py
+++ chapter6/6_7_get_words_start_a.py
@@ -33,4 +33,40 @@ with open('chapter6/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as 
 57.88 c. - вместе с загрузкой всех строк в память
 
 '''
+'''
+Файл содержит:
 
+A'Aang_NOUN	1879	45	5
+A'Aang_NOUN	1882	5	4
+A'Aang_NOUN	1885	1	1
+A'Aang_NOUN	1891	1	1
+A'Aang_NOUN	1899	20	4
+A'Aang_NOUN	1927	3	1
+A'Aang_NOUN	1959	5	2
+A'Aang_NOUN	1962	2	2
+A'Aang_NOUN	1963	1	1
+A'Aang_NOUN	1966	45	13
+A'Aang_NOUN	1967	6	4
+A'Aang_NOUN	1968	5	4
+A'Aang_NOUN	1970	6	2
+A'Aang_NOUN	1975	4	1
+A'Aang_NOUN	2001	1	1
+A'Aang_NOUN	2004	3	1
+A'que_ADJ	1808	1	1
+A'que_ADJ	1849	2	1
+A'que_ADJ	1850	1	1
+A'que_ADJ	1852	4	3
+A'que_ADJ	1854	5	3
+A'que_ADJ	1856	2	1
+A'que_ADJ	1858	4	3
+A'que_ADJ	1862	2	1
+A'que_ADJ	1871	1	1
+A'que_ADJ	1872	2	2
+A'que_ADJ	1873	1	1
+A'que_ADJ	1874	2	2
+A'que_ADJ	1875	3	3
+A'que_ADJ	1877	1	1
+A'que_ADJ	1881	1	1
+A'que_ADJ	1883	1	1
+
+'''
