deb5a284fec5f90663526d89ceac395778379a7c
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-27 00:46:11 +0500

    7_18

---
+++ chapter7/7_18_numpy_calc_avg_from_very_big_matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+import time
+
+data_points = 400000000
+rows = 50
+columns = int(data_points / rows)
+
+matrix = np.arange(data_points).reshape(rows, columns)
+
+s = time.time()
+
+res = np.mean(matrix, axis=1)
+
+e = time.time()
+print(e - s)  # 0.42987537384033203 c. (вщ го такой код выполняется за 285.690921ms)
