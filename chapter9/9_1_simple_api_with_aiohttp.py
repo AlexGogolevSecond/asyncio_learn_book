d6f292ac22798bb1ff3a1fabd28c239a7766e258
Alrksandr Gogolev <a.gogolev@eteron.ru>
2024-08-10 00:27:21 +0500

    9.1

--- chapter9/9_1_simple_api_with_aiohttp.py
+++ chapter9/9_1_simple_api_with_aiohttp.py
@@ -18,7 +18,12 @@ async def time(request: Request) -> Response:
 
     return web.json_response(result)  # B
 
+@routes.get('/')
+async def get_data(request: Request) -> Response:
+    shared_data = request.app['shared_dict']
+    return web.json_response(shared_data)
 
 app = web.Application()  # C
+app['shared_dict'] = {'key' : 'value'}
 app.add_routes(routes)
 web.run_app(app)
