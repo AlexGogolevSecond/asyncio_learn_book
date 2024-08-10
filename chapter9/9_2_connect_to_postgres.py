a314e3a2a3e8ffa88c4e99b7e9f5ed10402b863a
Aleksandr <a.gogolev@carbonsoft.ru>
2024-08-10 21:28:35 +0500

    9,2

--- chapter9/9_2_connect_to_postgres.py
+++ chapter9/9_2_connect_to_postgres.py
@@ -7,19 +7,26 @@ from asyncpg import Record
 from asyncpg.pool import Pool
 from typing import List, Dict
 
+from chapter5.connection import DATABASE_URL
+
 routes = web.RouteTableDef()
 DB_KEY = 'database'
 
 
 async def create_database_pool(app: Application): #A
     print('Creating database pool.')
-    pool: Pool = await asyncpg.create_pool(host='127.0.0.1',
-                                           port=5432,
-                                           user='postgres',
-                                           password='password',
-                                           database='products',
-                                           min_size=6,
-                                           max_size=6)
+    # pool: Pool = await asyncpg.create_pool(host='127.0.0.1',
+    #                                        port=5432,
+    #                                        user='postgres',
+    #                                        password='password',
+    #                                        database='products',
+    #                                        min_size=6,
+    #                                        max_size=6)
+
+    pool: Pool = await asyncpg.create_pool(min_size=6,
+                                           max_size=6,
+                                           **DATABASE_URL)
+
     app[DB_KEY] = pool
 
 
@@ -44,3 +51,6 @@ app.on_cleanup.append(destroy_database_pool)
 
 app.add_routes(routes)
 web.run_app(app)
+
+# запуск скрипта: (env) alex@ubuntu-home:~/py_tmp/asyncio_learn_book$ python -m chapter9.9_2_connect_to_postgres.py
+# в этом случае корректно распознаются абсолютные пути - в данном случае для получения параметров из DATABASE_URL
