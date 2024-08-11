a04711d429e5bd09fa68bcfcbd5ca16a0dec2213
Aleksandr <a.gogolev@carbonsoft.ru>
2024-08-12 00:06:19 +0500

    9,7

--- chapter9/listing_9_7_simple_asgi.py
+++ chapter9/listing_9_7_simple_asgi.py
@@ -5,3 +5,8 @@ async def application(scope, receive, send):
         'headers': [[b'content-type', b'text/html']]
     })
     await send({'type': 'http.response.body', 'body': b'ASGI hello!'})
+
+# запуск uvicorn chapter_09.listing_9_7:application
+
+# напрямую uvicorn не рекомендуется запускать - рекомендуется запускать uvicorn с помощью gunicorn,
+# т.к. в Gunicorn встроена логика автоматического перезапуска рабочих процессов в случае аварии
