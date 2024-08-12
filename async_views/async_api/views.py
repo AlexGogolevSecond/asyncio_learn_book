e1d10e81b6914ea92cedb8f6942d036123329b4e
Aleksandr <a.gogolev@carbonsoft.ru>
2024-08-13 01:02:12 +0500

    9,11

--- async_views/async_api/views.py
+++ async_views/async_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# listing_9_11
 
-# Create your views here.
+import asyncio
+from datetime import datetime
+from aiohttp import ClientSession
+from django.shortcuts import render
+import aiohttp
+
+async def get_url_details(session: ClientSession, url: str):
+    start_time = datetime.now()
+    response = await session.get(url)
+    response_body = await response.text()
+
+
+
+
