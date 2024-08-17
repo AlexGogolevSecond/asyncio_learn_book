ae94447efce0d6dc4b6d3083eeb1f7b3ee23d1e7
Aleksandr <a.gogolev@carbonsoft.ru>
2024-08-17 19:41:05 +0500

    10,9 10,10

---
+++ chapter10/listing_10_10_testing_retry.py
@@ -0,0 +1,29 @@
+import asyncio
+from chapter10.listing_10_9_retry_wait_for_if_exception import retry, TooManyRetries
+
+
+async def main():
+    async def always_fail():
+        raise Exception("I've failed!")
+
+    async def always_timeout():
+        await asyncio.sleep(1)
+
+    try:
+        await retry(always_fail,
+                    max_retries=3,
+                    timeout=.1,
+                    retry_interval=.1)
+    except TooManyRetries:
+        print('Retried too many times!')
+
+    try:
+        await retry(always_timeout,
+                    max_retries=3,
+                    timeout=.1,
+                    retry_interval=.1)
+    except TooManyRetries:
+        print('Retried too many times!')
+
+
+asyncio.run(main())
