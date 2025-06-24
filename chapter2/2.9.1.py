40d6d671195f7d39f9f1cd028e1c3daa424ae430
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-06-24 07:38:39 +0500

    2.10

--- chapter2/2.9.1.py
+++ chapter2/2.9.1.py
@@ -10,16 +10,16 @@ async def sl(n) -> None:
 
 async def main():
     start = time.time()
-    # t1 = asyncio.create_task(sl(3))
-    # t2 = asyncio.create_task(sl(4))
-    # await t1
-    # await t2
+    t1 = asyncio.create_task(sl(3))
+    t2 = asyncio.create_task(sl(4))
+    await t1
+    await t2
     # await asyncio.create_task(sl(3))
     # await asyncio.create_task(sl(4))
     
     # await asyncio.gather(t1, t2)  # можно и корутины передавать и задачи
     
-    await asyncio.gather(sl(3), sl(4))
+    # await asyncio.gather(sl(3), sl(4))
 
     # await sl(3)  # смысла вызывать через await сопрограмму нет, т.к. сопрограмма будет выполняться пока не выполнится, т.е.
     # await sl(4)  # обычное синхронное выполнение. Т.е. на каждой такой строке родительская сопрограмма приостанавливается
