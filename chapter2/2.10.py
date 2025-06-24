aa50ba0ca3bf4ca8d8a0cb158d1dfd4983013da6
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-06-24 08:04:37 +0500

    2.10

--- chapter2/2.10.py
+++ chapter2/2.10.py
@@ -1,5 +1,6 @@
 import asyncio
 from util import delay
+from datetime import datetime
 
 
 async def hello_every_second():
@@ -9,10 +10,16 @@ async def hello_every_second():
 
 
 async def main():
+    print(f"Создаём first_delay в {datetime.now().time()}")
     first_delay = asyncio.create_task(delay(3))
+    print(f"Создаём second_delay в {datetime.now().time()}")
     second_delay = asyncio.create_task(delay(3))
+    
     await hello_every_second()
+    
+    print(f"Ждём first_delay в {datetime.now().time()}")
     await first_delay
+    print(f"Ждём second_delay в {datetime.now().time()}")
     await second_delay
 
 asyncio.run(main())
