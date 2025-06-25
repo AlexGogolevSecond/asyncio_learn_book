db560ed5cafb9eedb1628953436c7c91c4bc4f27
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-06-25 08:17:55 +0500

    2.9 comment

--- chapter2/2.9 gather .py
+++ chapter2/2.9 gather .py
@@ -13,11 +13,11 @@ async def main():
     t1 = asyncio.create_task(sl(3))
     t2 = asyncio.create_task(sl(4))
     # await t1
-    # await t2
+    # await t2  # время выполнения 4 с, т.е. конкурентно
     # await asyncio.create_task(sl(3))
     # await asyncio.create_task(sl(4))
     
-    await asyncio.gather(t1, t2)  # можно и корутины передавать и задачи
+    # await asyncio.gather(t1, t2)  # можно и корутины передавать и задачи
     # await asyncio.gather(sl(3), sl(4))
 
     finish = time.time()
