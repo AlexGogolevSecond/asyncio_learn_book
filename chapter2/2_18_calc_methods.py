241eccbe9b78d67133142c985495595269eba33f
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-06-26 00:39:59 +0500

    123

---
+++ chapter2/2_18_calc_methods.py
@@ -0,0 +1,26 @@
+import asyncio
+# from ..util import async_timed
+import sys
+from pathlib import Path
+
+# Добавляем корень проекта в путь
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+# Абсолютный импорт
+from util.async_timer import async_timed
+
+@async_timed()
+async def count(n: int) -> int:
+    counter = 0
+    for i in range(n):
+        counter += 1
+    return counter
+
+@async_timed()
+async def main():
+    task_one = asyncio.create_task(count(100000000))
+    task_two = asyncio.create_task(count(100000000))
+    await task_one
+    await task_two
+
+asyncio.run(main())
