076369a922af3fbedb8ff8be1ffdd134f10b7095
Aleksandr <a.gogolev@carbonsoft.ru>
2025-06-28 09:47:22 +0500

    3,1 комменты

--- chapter3/3.1_socket.py
+++ chapter3/3.1_socket.py
@@ -1,9 +1,9 @@
 import socket
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создать TCP-сервер
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_address = ('127.0.0.1', 15454)
+server_address = ('127.0.0.1', 15454)  # задаём адрес сокета
 server_socket.bind(server_address)
 server_socket.listen()
 
