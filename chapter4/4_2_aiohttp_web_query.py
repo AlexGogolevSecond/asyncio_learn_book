46d55f2c576ce44ea0c63236f0eb0b342699c05d
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-03 19:02:18 +0500

    4,2

--- chapter4/4_2_aiohttp_web_query.py
+++ chapter4/4_2_aiohttp_web_query.py
@@ -24,7 +24,7 @@ async def main():
     async with aiohttp.ClientSession() as session:
         url = 'https://www.example.com'
         status = await fetch_status(session, url)
-        print(f'Состояние для {url} было равно {status}')
+        print(f'status для {url}: {status}')
 
 
 asyncio.run(main())
