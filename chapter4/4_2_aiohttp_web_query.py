4139889b25fd2aac8b1c633c473a0a7b01e033e2
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-03 19:00:56 +0500

    4,2

--- chapter4/4_2_aiohttp_web_query.py
+++ chapter4/4_2_aiohttp_web_query.py
@@ -2,7 +2,15 @@
 import asyncio
 import aiohttp
 from aiohttp import ClientSession
-from util import async_timed
+# from util import async_timed
+import sys
+from pathlib import Path
+
+# Добавляем корень проекта в путь
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+# Абсолютный импорт
+from util.async_timer import async_timed
 
 
 @async_timed()
