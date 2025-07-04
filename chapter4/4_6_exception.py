a7584601942306404f6a1f8e5dc8d5ee4d06899d
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-04 10:56:34 +0500

    4,6

--- chapter4/4_6_exception.py
+++ chapter4/4_6_exception.py
@@ -17,15 +17,15 @@ from util.fetch_status import fetch_status
 @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
-        urls = ['https://example.com', 'python://example.com', 'python://google.com']
+        urls = ['https://example.com', 'python://example.com', 'https://google.com']
         tasks = [fetch_status(session, url) for url in urls]
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
         exceptions = [res for res in results if isinstance(res, Exception)]
         successful_results = [res for res in results if not isinstance(res, Exception)]
         print('*' * 25)
-        print(f'Все результаты: {results}')
-        print(f'Завершились успешно: {successful_results}')
-        print(f'Завершились с исключением: {exceptions}')
+        print(f'Все результаты: {results=}')
+        print(f'Завершились успешно: {successful_results=}')
+        print(f'Завершились с исключением: {exceptions=}')
 
 asyncio.run(main())
