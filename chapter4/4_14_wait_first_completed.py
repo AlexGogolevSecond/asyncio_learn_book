160698efd199ca93beb7f62108dfd1b9bdf7ab18
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-07-09 00:23:20 +0500

    4,14

--- chapter4/4_14_wait_first_completed.py
+++ chapter4/4_14_wait_first_completed.py
@@ -1,6 +1,14 @@
 import aiohttp
 import asyncio
-from util import async_timed, fetch_status
+import sys
+from pathlib import Path
+
+# Добавляем корень проекта в путь
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+# Абсолютный импорт
+from util.async_timer import async_timed
+from util.fetch_status import fetch_status
 
 
 @async_timed()
@@ -12,11 +20,10 @@ async def main():
                    asyncio.create_task(fetch_status(session, url))]
         
         while pending:
-            done, pending = await asyncio.wait(pending,
-                                               return_when=asyncio.FIRST_COMPLETED)
+            done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
             print(f'Число завершившихся задач: {len(done)}')
             print(f'Число ожидающих задач: {len(pending)}')
-        for done_task in done:
-            print(await done_task)
+            for done_task in done:
+                print(await done_task)
 
 asyncio.run(main())
