2dc3e3b925a9849ecf1a2d62e746142dffd56b58
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-07-09 22:38:45 +0500

    connection

--- chapter5/connection.py
+++ chapter5/connection.py
@@ -1,13 +1,13 @@
 host='127.0.0.1'
-port=19432
+port=5432
 user='postgres'
 database='products'
-password='password'
+password='123'
 
 # DATABASE_URL = f'postgresql://{user}:{password}@{host}:{port}'  # почему-то не робит в 5_3
 DATABASE_URL = {'host': '127.0.0.1',
-                 'port': 19432,
-                 'user': 'postgres',
-                 'database': 'products',
-                 'password': 'password'
-                }
+                'port': 5432,
+                'user': 'postgres',
+                'database': 'products',
+                'password': '123'
+}
