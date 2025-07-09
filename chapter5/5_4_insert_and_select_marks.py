005e4abca9f9e1c4a56c07a711278aa98b1feaa9
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-07-10 00:06:00 +0500

    5.4

--- chapter5/5_4_insert_and_select_marks.py
+++ chapter5/5_4_insert_and_select_marks.py
@@ -7,18 +7,18 @@ from connection import DATABASE_URL
 
 async def main():
     connection = await asyncpg.connect(**DATABASE_URL)
-    
+
     # await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Levis')")
     # await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Seven')")
-    
+
     brand_query = 'SELECT brand_id, brand_name FROM brand'
-    results: List[Record] = await connection.fetch(brand_query)  # выбирает все записи
-    
-    if not results:
+
+    if not await connection.fetch(brand_query):
         await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Levis')")
         await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Seven')")
     results: List[Record] = await connection.fetch(brand_query)  # выбирает все записи
     print(f'{results=}')
+    # results=[<Record brand_id=1 brand_name='Levis'>, <Record brand_id=2 brand_name='Seven'>]
     for brand in results:
         print(f'id: {brand["brand_id"]}, name: {brand["brand_name"]}')
 
