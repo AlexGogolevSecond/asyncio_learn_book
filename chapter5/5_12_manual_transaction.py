5458e1a0b2d2691daeb3c4dea05dff1edb6990ac
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-12 21:05:53 +0500

    5,12

--- chapter5/5_12_manual_transaction.py
+++ chapter5/5_12_manual_transaction.py
@@ -6,7 +6,7 @@ from connection import DATABASE_URL
 
 async def main():
     connection = await asyncpg.connect(**DATABASE_URL)
-    transaction: Transaction = connection.transaction()
+    transaction: Transaction = connection.transaction()  # создаем объект транзакции, но без контекстного менеджера
     await transaction.start()
 
     try:
