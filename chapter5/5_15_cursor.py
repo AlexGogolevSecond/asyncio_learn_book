199e5db25a884c21a23de805fe928ed1c2f9f8ae
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-12 23:42:55 +0500

    5,15

--- chapter5/5_15_cursor.py
+++ chapter5/5_15_cursor.py
@@ -6,7 +6,8 @@ from connection import DATABASE_URL
 async def main():
     connection = await asyncpg.connect(**DATABASE_URL)
 
-    query = 'SELECT product_id, product_name FROM product'
+    # query = 'SELECT product_id, product_name FROM product'
+    query = 'select * from brand'
     async with connection.transaction():  # обязательно д.б. транзакция
         async for product in connection.cursor(query):  # выбираем по 50 (по умолчанию https://magicstack.github.io/asyncpg/current/api/index.html)
             print(f'{product=}')
