b925bfa1e85022e29a6d85925a1bf0dd7280ef47
Aleksandr <a.gogolev@carbonsoft.ru>
2025-07-12 23:20:31 +0500

    5,14

--- chapter5/5_14_simple_async_generator.py
+++ chapter5/5_14_simple_async_generator.py
@@ -1,23 +1,26 @@
 import asyncio
 #from util import delay, async_timed
 import time
+import sys
+from pathlib import Path
 
+# Добавляем корень проекта в путь
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-async def delay(delay_seconds: int) -> int:
-    print(f'засыпаю на {delay_seconds} с')
-    await asyncio.sleep(delay_seconds)
-    print(f'сон в течение {delay_seconds} с закончился')
-    return delay_seconds
+# Абсолютный импорт
+from util.async_timer import async_timed
+from util.fetch_status import fetch_status
+from util.delay_functions import delay
 
 
 async def positive_integers_async(until: int):
     for integer in range(1, until):
         # await delay(3)
-        await delay(integer)
+        await delay(until)
         yield integer
 
 
-#@async_timed()
+@async_timed()
 async def main():
     async_generator111 = positive_integers_async(3)  # type async_generator
     print(type(async_generator111))
