902162a599ba343f097b2a1c97e4df8a6f7de0d1
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-07-19 08:33:12 +0500

    6.11

--- chapter6/6_11_common_value.py
+++ chapter6/6_11_common_value.py
@@ -1,13 +1,16 @@
 from multiprocessing import Process, Value, cpu_count
+import os
 
 
 def increment_value(shared_int: Value):
+    print(f'{os.getpid()=}')
     shared_int.value = shared_int.value + 1
+    print(f'{shared_int.value=}')
 
 
 if __name__ == '__main__':
     print(f'{cpu_count()=}')
-    for _ in range(100):
+    for _ in range(10):
         integer = Value('i', 0)
         procs = [Process(target=increment_value, args=(integer,)),
                  Process(target=increment_value, args=(integer,))]
