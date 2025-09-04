9c9d01171e1b7c6ae6446b4b31e587fe79def20c
Aleksandr <a.gogolev@carbonsoft.ru>
2025-09-04 15:34:56 +0500

    7,5

--- chapter7/listing_7_5_requests_pool_gather.py
+++ chapter7/listing_7_5_requests_pool_gather.py
@@ -16,7 +16,7 @@ async def main():
     loop = asyncio.get_running_loop()
     with ThreadPoolExecutor(max_workers=1000) as pool:  # без указания max_workers выполняется примерно за 25 секунд, с max_workers=1000 за 2,5 секунды
         urls = ['https://www.example.com' for _ in range(1000)]
-        tasks = [loop.run_in_executor(pool, functools.partial(get_status_code, url)) for url in urls]
+        tasks = [loop.run_in_executor(pool, functools.partial(get_status_code, url)) for url in urls]  # странная конструкция
         results = await asyncio.gather(*tasks)
         # print(results)
 
