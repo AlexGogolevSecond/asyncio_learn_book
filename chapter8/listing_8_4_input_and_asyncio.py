d6069fe5b52f5792464fb55ada50792363596b20
Alrksandr Gogolev <a.gogolev@eteron.ru>
2025-10-26 01:02:33 +0500

    8.4

--- chapter8/listing_8_4_input_and_asyncio.py
+++ chapter8/listing_8_4_input_and_asyncio.py
@@ -2,10 +2,16 @@ import asyncio
 # from ..delay_functions import 
 
 
+async def delay(delay_seconds: int) -> int:
+    print(f'засыпаю на {delay_seconds} с')
+    await asyncio.sleep(delay_seconds)
+    print(f'сон в течение {delay_seconds} с закончился')
+    return delay_seconds
+
 async def main():
     while True:
         delay_time = input('Enter a time to sleep:')
-        asyncio.create_task(asyncio.sleep(int(delay_time)))
+        asyncio.create_task(delay(int(delay_time)))
 
 
 asyncio.run(main())
