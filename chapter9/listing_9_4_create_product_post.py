cb906acf15da38f696884c08f6d798358aa5c683
alex <a@gmail.com>
2025-11-02 03:28:40 +0500

    9.4

--- chapter9/listing_9_4_create_product_post.py
+++ chapter9/listing_9_4_create_product_post.py
@@ -2,17 +2,45 @@ from aiohttp import web
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
 from chapter9.listing_9_2_connect_to_postgres import create_database_pool, destroy_database_pool
+from asyncpg.pool import Pool
+from asyncpg import Record
 
 
 routes = web.RouteTableDef()
 DB_KEY = 'database'
 
 
+@routes.get('/products/{id}')
+async def get_product(request: Request) -> Response:
+    try:
+        str_id = request.match_info['id'] #A
+        product_id = int(str_id)
+
+        query = """
+            SELECT
+                product_id,
+                product_name,
+                brand_id
+            FROM product
+            WHERE product_id = $1
+        """
+
+        connection: Pool = request.app[DB_KEY]
+        result: Record = await connection.fetchrow(query, product_id) #B
+
+        if result is not None: #C
+            return web.json_response(dict(result))
+        else:
+            raise web.HTTPNotFound()  # 404
+    except ValueError:
+        raise web.HTTPBadRequest()  # 400
+
 @routes.post('/product')
 async def create_product(request: Request) -> Response:
     PRODUCT_NAME = 'product_name'
     BRAND_ID = 'brand_id'
 
+    print(f'{request.text=}')
     if not request.can_read_body:
         raise web.HTTPBadRequest()
 
@@ -28,7 +56,7 @@ async def create_product(request: Request) -> Response:
                          int(body[BRAND_ID]))
         return web.Response(status=201)
     else:
-        raise web.HTTPBadRequest()
+        raise web.HTTPBadRequest(text='Какая-то ерунда в теле запроса!')
 
 
 app = web.Application()
@@ -36,4 +64,4 @@ app.on_startup.append(create_database_pool)
 app.on_cleanup.append(destroy_database_pool)
 
 app.add_routes(routes)
-web.run_app(app)
+web.run_app(app, port=50000)
