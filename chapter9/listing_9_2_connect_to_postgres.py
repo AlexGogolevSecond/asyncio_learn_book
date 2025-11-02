ba1c9ed75832879fb01c3ca1b0189fa35e26dd78
alex <a@gmail.com>
2025-11-02 05:00:30 +0500

    сравнение фласк и aiohttp - aiohttp не сильно то и выигрывает

--- chapter9/listing_9_2_connect_to_postgres.py
+++ chapter9/listing_9_2_connect_to_postgres.py
@@ -23,9 +23,14 @@ async def create_database_pool(app: Application): #A
     #                                        min_size=6,
     #                                        max_size=6)
 
-    pool: Pool = await asyncpg.create_pool(min_size=6,
-                                           max_size=6,
-                                           **DATABASE_URL)
+    pool: Pool = await asyncpg.create_pool(
+        min_size=30,           # Компромиссный вариант
+        max_size=200,           # Чуть меньше максимума
+        max_inactive_connection_lifetime=60,
+        command_timeout=30,    # Таймаут на запросы
+        **DATABASE_URL)
+    
+    # !!! в постгрес поменял значение max_connections = 500 и перезапустил докер контейнер с постгресом
 
     app[DB_KEY] = pool
 
@@ -39,7 +44,7 @@ async def destroy_database_pool(app: Application): #B
 @routes.get('/brands')
 async def brands(request: Request) -> Response: #C
     pool: Pool = request.app[DB_KEY]
-    brand_query = 'SELECT brand_id, brand_name FROM brand LIMIT 10'
+    brand_query = 'SELECT brand_id, brand_name FROM brand'
     results: List[Record] = await pool.fetch(brand_query)
     result_as_dict: List[Dict] = [dict(brand) for brand in results]
     return web.json_response(result_as_dict)
@@ -50,9 +55,9 @@ app.on_startup.append(create_database_pool)  # сопрограмма create_dat
 app.on_cleanup.append(destroy_database_pool)
 
 app.add_routes(routes)
-web.run_app(app, port=50000)
+# web.run_app(app, access_log=None, print=None)  # убрал, т.к. запускаю через gunicorn -w 8 -k aiohttp.GunicornWebWorker -b localhost:8080 chapter9.listing_9_2_connect_to_postgres:app
 
-# запуск скрипта: (env) alex@ubuntu-home:~/py_tmp/asyncio_learn_book$ python -m chapter9._9_2_connect_to_postgres.py
+# запуск скрипта: (env) alex@ubuntu-home:~/py_tmp/asyncio_learn_book$ python -m chapter9.listing_9_2_connect_to_postgres.py
 # в этом случае корректно распознаются абсолютные пути - в данном случае для получения параметров из DATABASE_URL
 
 '''
@@ -70,3 +75,35 @@ Transfer/sec:      5.98MB
 тут на одном процессе в одном потоке 1500 рпс, а на фласке с 4-мя процессами 3000 рпс. Хотя на фласке в htop почему то
 задействуются все 8 ядер, хотя я указывал гуникорну 4 ядра. На 8 ядрах у фласка 4000 рпс.
 '''
+
+# на ноуте в wsl:
+# один проесс - капец медленно
+# wrk -t1 -c200 -d30s http://localhost:8080/brands
+# Running 30s test @ http://localhost:8080/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency   151.30ms  251.44ms   1.99s    95.27%
+#     Req/Sec     2.01k   361.52     2.43k    90.18%
+#   57291 requests in 28.38s, 222.59MB read
+#   Socket errors: connect 0, read 0, write 0, timeout 218
+# Requests/sec:   2018.49
+# Transfer/sec:      7.84MB
+
+# 8 процессов/воркеров (гуникорн):
+# wrk -t1 -c200 -d30s http://localhost:8080/brands
+# Running 30s test @ http://localhost:8080/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency    43.17ms   39.02ms   1.53s    81.85%
+#     Req/Sec     4.84k   785.34     6.28k    68.00%
+#   145063 requests in 29.18s, 563.61MB read
+#   Socket errors: connect 0, read 0, write 0, timeout 345
+# Requests/sec:   4971.51
+# Transfer/sec:     19.32MB
+
+
+
+# Конфигурация	Req/Sec	Прирост vs Flask
+# Flask + gunicorn	4027	базовый
+# aiohttp + gunicorn (min=50)	4426	+10%
+# aiohttp + gunicorn (min=30)	4971	+23%
--- chapter9/listing_9_5_flask_get_from_pg.py
+++ chapter9/listing_9_5_flask_get_from_pg.py
@@ -4,8 +4,8 @@ from chapter5.connection import DATABASE_URL
 
 app = Flask(__name__)
 
-conn_info = "dbname=products user=postgres password=password host=127.0.0.1 port=19432"
-db = psycopg2.connect(DATABASE_URL)
+conn_info = "dbname=products user=postgres password=password host=127.0.0.1 port=6432"
+db = psycopg2.connect(conn_info)
 
 
 @app.route('/brands')
@@ -20,16 +20,58 @@ def brands():
 # нагрузочный тест:
 # wrk -t1 -c200 -d30s http://localhost:8000/brands - в теч. 30 с. в одном потоке 200 подключений 
 
-'''
-Вывод:
-wrk -t1 -c200 -d30s http://localhost:8000/brands
-Running 30s test @ http://localhost:8000/brands
-  1 threads and 200 connections
-  Thread Stats   Avg      Stdev     Max   +/- Stdev
-    Latency    63.09ms    4.04ms 106.76ms   96.69%
-    Req/Sec     3.17k   159.12     3.36k    95.00%
-  94678 requests in 30.01s, 330.56MB read
-Requests/sec:   3154.57
-Transfer/sec:     11.01MB
 
-''' 
+# Вывод:
+# wrk -t1 -c200 -d30s http://localhost:8000/brands
+# Running 30s test @ http://localhost:8000/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency    63.09ms    4.04ms 106.76ms   96.69%
+#     Req/Sec     3.17k   159.12     3.36k    95.00%
+#   94678 requests in 30.01s, 330.56MB read
+# Requests/sec:   3154.57
+# Transfer/sec:     11.01MB
+
+# на ноуте:
+# flask:
+# alex@hp-laptop:~$ wrk -t1 -c200 -d30s http://localhost:8000/brands
+# Running 30s test @ http://localhost:8000/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency    52.27ms    9.72ms 158.81ms   88.97%
+#     Req/Sec     3.82k   583.95     4.80k    80.60%
+#   114327 requests in 28.39s, 399.16MB read
+#   Socket errors: connect 0, read 0, write 0, timeout 200
+# Requests/sec:   4027.62
+# Transfer/sec:     14.06MB
+
+# aiohttp:
+# на ноуте в wsl:
+# wrk -t1 -c200 -d30s http://localhost:8080/brands
+# Running 30s test @ http://localhost:8080/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency   120.33ms   45.90ms 635.07ms   83.99%
+#     Req/Sec     1.69k   310.30     2.05k    85.95%
+#   50531 requests in 28.41s, 196.33MB read
+#   Socket errors: connect 0, read 0, write 0, timeout 200
+# Requests/sec:   1778.93
+# Transfer/sec:      6.91MBB
+
+# 8 процессов/воркеров (гуникорн) aiohttp:
+# wrk -t1 -c200 -d30s http://localhost:8080/brands
+# Running 30s test @ http://localhost:8080/brands
+#   1 threads and 200 connections
+#   Thread Stats   Avg      Stdev     Max   +/- Stdev
+#     Latency    43.17ms   39.02ms   1.53s    81.85%
+#     Req/Sec     4.84k   785.34     6.28k    68.00%
+#   145063 requests in 29.18s, 563.61MB read
+#   Socket errors: connect 0, read 0, write 0, timeout 345
+# Requests/sec:   4971.51
+# Transfer/sec:     19.32MB
+
+# 
+# Конфигурация	Req/Sec	Прирост vs Flask
+# Flask + gunicorn	4027	базовый
+# aiohttp + gunicorn (min=50)	4426	+10%
+# aiohttp + gunicorn (min=30)	4971	+23%
