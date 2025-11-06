11dba768a9ed1c5ac6d30b8494eab34984d961fb
alex <a@gmail.com>
2025-11-06 23:22:16 +0500

    comment

--- chapter10/listing_10_1_get_inventory_by_id_product.py
+++ chapter10/listing_10_1_get_inventory_by_id_product.py
@@ -1,18 +1,20 @@
 import asyncio
 import random
-from aiohttp import web
+
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
+
+from aiohttp import web
 
 routes = web.RouteTableDef()
 
 
-@routes.get('/products/{id}/inventory')
+@routes.get("/products/{id}/inventory")
 async def get_inventory(request: Request) -> Response:
     delay: float = random.randint(0, 20) / 10
     await asyncio.sleep(delay)
-    inventory: int = random.randint(0, 100)
-    return web.json_response({'inventory': inventory})
+    inventory: int = random.randint(0, 100)  # непонятно - зачем, вроде как для примера
+    return web.json_response({"inventory": inventory})
 
 
 app = web.Application()
